405deca3e594ea7288cb9fdf22bfb5e8e91e659d
Cristianetaniguti <chaytaniguti@gmail.com>
2025-10-11 12:25:06 -0400

    trying again

--- R/standardization.R
+++ R/standardization.R
@@ -572,6 +572,7 @@ standardize <- function(data = NULL,
     if(verbose) cat("Going to parallel mode...\n")
     clust <- makeCluster(n.cores, type = parallel.type)
     clusterExport(clust, c("get_centers", "rm_outlier"), envir = .GlobalEnv)
+    clusterEvalQ(clust, { library(magrittr); library(dplyr)}) # load required packages and Qploidy on workers
     clusters <- parLapply(clust, lst_standardization, get_centers,
                           ploidy= ploidy.standardization,
                           n.clusters.thr = threshold.n.clusters,
